de544fc29ff7cb9756d72a4c206cb0c1c85f0042
Felipe <felipecruz.estudo@gmail.com>
2026-04-14 20:19:33 -0300

    Alteração em principais demanda. Adicionado as funcionalidades de botão.

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ behavior:"smooth"
 })
 
 })
+
+const demandas = document.querySelectorAll('.demanda-item');
+
+demandas.forEach(item => {
+    item.addEventListener('click', () => {
+
+        const ativo = document.querySelector('.demanda-item.active');
+
+        if(ativo && ativo !== item){
+            ativo.classList.remove('active');
+        }
+
+        item.classList.toggle('active');
+    });
+});
+
+
+document.querySelectorAll('.item-toggle').forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('active');
+    });
+});
